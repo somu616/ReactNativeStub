c3abb838eb3e64d9c4bb78fe74bee436a67a86bd
somu616 <somujj@gmail.com>
2018-07-03 08:02:17 -0500

    removing other scenes from router

--- App.js
+++ App.js
@@ -14,8 +14,6 @@ import {
 } from 'react-native';
 import {Scene, Reducer, Router, Switch, TabBar, Modal, Actions} from 'react-native-router-flux';
 import Login from './components/Login';
-import CameraImageGallery from './components/CameraGalleryImages';
-import ImageCaption from './components/ImageCaption';
 var EventEmitter = require('EventEmitter');
 var Subscribable = require('Subscribable');
 import Store from './util/store.js';
@@ -40,24 +38,11 @@ export default class App extends Component {
     componentWillMount() {
         this.eventEmitter = new EventEmitter();
     }
-    sendImageToCaptionPage() {
-        //Actions.imageCaption();
-        this.eventEmitter.emit("imageSelected");
-    }
-    showSubmitButton() {
-        return(
-            <TouchableHighlight underlayColor="#ffffff" style={{paddingRight:10}} onPress={this.sendImageToCaptionPage.bind(this)}>
-                <Text style={{color:"white",borderRadius:10,padding:5,fontColor:'gray',fontSize:20,fontWeight:'bold',backgroundColor:'#0a6464'}}>SUBMIT</Text>
-            </TouchableHighlight>
-        )
-    }
   render() {
         var that=this;
     return (
         <Router createReducer={reducerCreate}>
             <Scene key="loginModal" component={Login} hideNavBar={true} />
-            <Scene key="cameraGalleryImages" type="replace" /*initial={true}*/  hideNavBar={false} title="Select Image" navigationBarStyle={{height:65}} titleStyle={{color:'black',textAlign:'left',marginLeft:70,fontWeight:'bold'}} events={that.eventEmitter} component={CameraImageGallery} renderRightButton={this.showSubmitButton.bind(this)}/>
-            <Scene key="imageCaption" /*initial={true}*/  hideNavBar={false} title="Add Caption" navigationBarStyle={{height:65}} titleStyle={{color:'black',textAlign:'left',marginLeft:70,fontWeight:'bold'}} events={that.eventEmitter} component={ImageCaption}/>
         </Router>
     );
   }
